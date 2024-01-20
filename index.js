e317b52ad1af6b911b906c3b2dab8658047fcdc9
Jacob Mbendera <jaybmbendera96@gmail.com>
2024-01-20 23:14:55 +0200

    contact us api endpoint for healix

--- index.js
+++ index.js
@@ -30,12 +30,6 @@ app.get('/', (req, res) => {
     Welcome_to: 'JACOB MBENDERA MAIL SERVICE',
   });
 });
-// app.get('/400', (req, res) => {
-//   res.sendStatus(400);
-// });
-// app.get('/500', (req, res) => {
-//   res.sendStatus(500);
-// });
 
 app.post('/contact', (req, res) => {
   const { firstName, lastName, email, mobile, message } = req.body;
@@ -81,6 +75,53 @@ app.post('/contact', (req, res) => {
     .send(msg)
     .then(() => {
       res.status(200).send('Sent, Jacob will contact you soon');
+    })
+    .catch((error) => {
+      res.status(500).send('An Error occured while sending');
+    });
+});
+
+// HEALIX MAIL SERVICE
+
+app.post('/contactUs', (req, res) => {
+  const { fullName, email, mobile, message } = req.body;
+
+  //vaidating email use RegEx
+  if (!validateEmail(email)) {
+    return res.status(400).json({
+      massage: 'Invalid Email',
+    });
+  }
+  if (!validate(mobile)) {
+    return res.status(400).json({
+      massage: 'Invalid Phone',
+    });
+  }
+  //validating length for diffent fields
+  if (!validateLength(fullName, 3, 30)) {
+    return res.status(400).json({
+      massage: 'Last Name must be between 3 and 30 characters',
+    });
+  }
+  if (!validateLength(message, 10, 40)) {
+    return res.status(400).json({
+      massage: 'massage must be between 10 and 40 characters',
+    });
+  }
+
+  const msg = {
+    to: ['jaybmbendera96@gmail.com'],
+    from: `${email}`,
+    subject: 'HEALIX MEDICAL CONTACT FORM',
+    html: `
+    <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title style="background-color:#044b63">CONTACT FORM - HEALIX MEDICAL OFFICIAL WEBSITE</title></head><body><div class="" style="max-width:700px;margin-bottom:1em;display:flex;align-items:center;gap:10px;font-family:Roboto;background-color:#044b63;padding:1em;border-radius:2px;margin:0 auto;text-align:center"><img src="https://res.cloudinary.com/dhbe6wtod/image/upload/v1705782886/healix/jzmmzdenz1ye8kuosnjz.png" alt="" style="width:50px"><span style="color:azure;text-transform:uppercase;text-align:center;font-size:large;margin:0 auto;font-weight:bolder">CONTACT FORM</span></div><div style="font-family:Roboto;font-size:17px;padding:1em;border-top:4px solid #fff;border-bottom:4px solid #afafaf;width:700px;margin:0 auto;background-color:#044b63;color:azure"><span style="font-weight:700;text-align:center">Hello</span><div style="padding:20px 0"><span style="padding:1.5em 0"><strong>Full Name</strong>:My name is ${fullName}<br><strong>Mobile Number</strong>: ${mobile}<br><strong>Email Address</strong>: ${email}<br></span><p>${message}</p></div><div style="padding-top:20px"><span style="margin:1.5em 0;color:azure">&copy; Healix Medical. All right reserved.</span></div></div></body></html>
+      `,
+  };
+
+  sgMail
+    .send(msg)
+    .then(() => {
+      res.status(200).send('Sent!, One of our members will contact you');
     })
     .catch((error) => {
       res.status(500).send('An Error occured while sending');
